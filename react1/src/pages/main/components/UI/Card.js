29759ae1f62b3049ddc0aa9dd0c3f5bef28b003c
sangh789 <sangh789@naver.com>
2022-04-28 17:29:25 +0900

    ~73강

---
+++ react1/src/pages/main/components/UI/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../../css/Card.css'
+
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+  return (
+    
+    <div className={classes}>
+      {props.children}
+    </div>
+  );
+
+}
+
+export default Card;
+
